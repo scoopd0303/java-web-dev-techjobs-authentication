3c13f7c2d6772ea705dcb4007e5dd48a580c2c7f
Scott McDonald <scottmcdonald@scotts-air.attlocal.net>
2021-07-01 19:29:29 -0500

    finished 19.6.3 parts 1-3

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/AuthenticationController.java
@@ -0,0 +1,38 @@
+package org.launchcode.javawebdevtechjobsauthentication.controllers;
+
+import org.launchcode.javawebdevtechjobsauthentication.models.User;
+import org.launchcode.javawebdevtechjobsauthentication.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+@Controller
+public class AuthenticationController {
+
+    @Autowired
+    public UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        return user.get();
+    }
+
+    private static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(userSessionKey, user.getId());
+    }
+
+}
--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/data/UserRepository.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/data/UserRepository.java
@@ -1,4 +1,12 @@
 package org.launchcode.javawebdevtechjobsauthentication.models.data;
 
-public interface UserRepository {
+import org.launchcode.javawebdevtechjobsauthentication.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUsername(String username);
+
 }
